86f12fd24edf050671017a1b1352537b3ee76bd8
lladlam <lladlam@outlook.com>
2026-08-09 05:48:35 +0800

    android: add shared player container transition key

---
+++ android/app/src/main/kotlin/com/lladlam/melox/ui/player/MeloXPlayerTransitionKeys.kt
@@ -0,0 +1,8 @@
+package com.lladlam.melox.ui.player
+
+internal data class MeloXPlayerContainerKey(
+    val mediaId: String,
+)
+
+internal fun sharedPlayerContainerKey(mediaId: String): MeloXPlayerContainerKey =
+    MeloXPlayerContainerKey(mediaId)
